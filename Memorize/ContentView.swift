fb3c6323fa9ebd760b7c6dd4ccbbc8fc008685b6
Romulo Messias <romulomessias@me.com>
2020-06-26 15:02:03 -0300

    Initial commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Romulo Messias on 25/06/20.
+//  Copyright © 2020 Romulo Messias. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
